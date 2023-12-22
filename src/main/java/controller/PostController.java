e32efe146039fc36ff0c29a1b803de6ba7b02e99
CalvinKuan <91074355+CalvinKuan@users.noreply.github.com>
2023-12-23 01:25:31 +0800

    新增取得單一文章功能

--- src/main/java/controller/PostController.java
+++ src/main/java/controller/PostController.java
@@ -64,14 +64,15 @@ public class PostController extends HttpServlet {
 		JsonReader jsr = new JsonReader(request);
 
 		// String id = jsr.getParameter("id");
-		String id = request.getParameter("member_id");
-		System.out.println("jsr.getparameter id: " + id);
+		String post_id = request.getParameter("post_id");
+		String member_id = request.getParameter("member_id");
+		System.out.println("jsr.getparameter id: " + member_id);
 
 		/** 若直接透過前端AJAX之data以key=value之字串方式進行傳遞參數，可以直接由此方法取回資料 */
 		// int id = -1;
 		// id = jso.getInt("member_id");
 		/** 判斷該字串是否存在，若存在代表要取回個別會員之資料，否則代表要取回全部資料庫內會員之資料 */
-		if (id == null) {
+		if (member_id == null && post_id == null) {
 			System.out.println("func: postcontroller get all post");
 			/** 透過MemberHelper物件之getAll()方法取回所有會員之資料，回傳之資料為JSONObject物件 */
 			JSONObject query = ph.getAllPost();
@@ -84,10 +85,25 @@ public class PostController extends HttpServlet {
 
 			/** 透過JsonReader物件回傳到前端（以JSONObject方式） */
 			jsr.response(resp, response);
-		} else {
+		}else if(post_id != null) {
+			
+			//System.out.println("func: postcontroller get by id");
+			
+			/** 透過MemberHelper物件的getByID()方法自資料庫取回該名會員之資料，回傳之資料為JSONObject物件 */
+			JSONObject query = ph.getSinglePost(post_id);
+
+			/** 新建一個JSONObject用於將回傳之資料進行封裝 */
+			JSONObject resp = new JSONObject();
+			resp.put("status", "200");
+			resp.put("message", "貼文資料取得成功");
+			resp.put("response", query);
+			System.out.println("pctrller do get response:" + resp);
+			/** 透過JsonReader物件回傳到前端（以JSONObject方式） */
+			jsr.response(resp, response);
+		}else {
 			System.out.println("func: postcontroller get by id");
 			/** 透過MemberHelper物件的getByID()方法自資料庫取回該名會員之資料，回傳之資料為JSONObject物件 */
-			JSONObject query = ph.getByID(id);
+			JSONObject query = ph.getByID(member_id);
 
 			/** 新建一個JSONObject用於將回傳之資料進行封裝 */
 			JSONObject resp = new JSONObject();
